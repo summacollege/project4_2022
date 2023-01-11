4dbe143a6eae826c489ea459b7a1b2482cbe29c4
PS227368 <ps227368@summacollege.nl>
2023-01-11 15:11:52 +0100

    werkt half ron vrijdag vragen

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
 class PasswordController extends Controller
