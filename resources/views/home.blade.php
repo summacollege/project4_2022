79053b852bd6b2475e1d044db9daf688e6561f38
PS227368 <ps227368@summacollege.nl>
2023-01-17 23:18:53 +0100

    23:19

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,9 +22,8 @@
         @endif
     @endforeach
                 @else
-                <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">Inloggen</a>
-          
-@endif
+                <a href="{{ route('login') }}" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">Inloggen</a>    
+    @endif
 
 
   </div>
