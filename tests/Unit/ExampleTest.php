6a798285f857666563ffeb90f40652e3cd27a039
ps233717 <93662389+ps233717@users.noreply.github.com>
2023-02-06 12:59:57 +0100

    unittest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,41 +14,4 @@ class HomePageTest extends TestCase
     }
 }
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class TrackAndTraceTest extends TestCase
-{
-    use WithoutMiddleware;
-
-    public function testTrackPageLoadsCorrectly()
-    {
-        $this->get('/tracktrace')
-             ->assertStatus(200)
-             ->assertSee('tracktrace');
-    }
-
-    public function testTraceWithInvalidTrackingNumber()
-    {
-        $response = $this->post('/tracktrace', [
-            'tracking_number' => 'invalid'
-        ]);
-
-        $response->assertSessionHasErrors();
-        $this->assertEquals(session()->get('errors')->first(), 'Invalid tracking number.');
-    }
-
-    public function testTraceWithValidTrackingNumber()
-    {
-        $response = $this->post('/tracktrace', [
-            'tracking_number' => 'valid'
-        ]);
-
-        $response->assertSee('Delivery status: Delivered');
-        $response->assertSee('Delivery date: 01/01/2022');
-        $response->assertSee('Delivery location: 123 Main St, Anytown USA');
-    }
-}
-
 
